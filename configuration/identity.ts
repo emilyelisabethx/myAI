62c3e5ad0726411ce1cb1589ea71ff285b191a28
Emily Dixon <emilyelisabethx@gmail.com>
2025-02-17 12:18:08 -0500

    Update identity (owner, description, ai name)

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Emily Dixon`;
+export const OWNER_DESCRIPTION: string = `UNC MBA Student`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Elisabeth`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
