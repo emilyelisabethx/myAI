43081b62a8d0115281e1fc42f22d39966b36bdec
Emily Dixon <emilyelisabethx@gmail.com>
2025-02-26 14:26:09 -0500

    Update identity.ts
    Changed ai bot name, and tone.

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Emily Dixon`;
-export const OWNER_DESCRIPTION: string = `UNC MBA Student`;
+export const OWNER_DESCRIPTION: string = `Bali Travel Agent`;
 
-export const AI_NAME: string = `Elisabeth`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_NAME: string = `Wayan`;
+export const AI_TONE: string = `friendly and personable`;
 export const AI_ROLE: string = `[AI ROLE]`;
